7043431fd076a7ff69edb45a73f0dc6641cb8e7e
JayMan146 <jeremywgrover@gmail.com>
2025-12-27 12:11:56 -0800

    indent statistics with tabs

--- statistics.py
+++ statistics.py
@@ -8,31 +8,31 @@ USER_SETTINGS: dict
 
 @dataclasses.dataclass
 class JournalStatistic:
-    title_word_count: int
-    body_word_count: int
-    word_rating: str
-    number_rating: int
-    weather: str
-    number_of_photos: int
+	title_word_count: int
+	body_word_count: int
+	word_rating: str
+	number_rating: int
+	weather: str
+	number_of_photos: int
 
 def main():
-    USER_SETTINGS = load_settings()
-    earliest_journal_dict: dict = USER_SETTINGS["earliest_journal"]
-    earliest_journal: datetime.date = datetime.date(earliest_journal_dict["year"], earliest_journal_dict["month"], earliest_journal_dict["day"])
-    today: datetime.date = datetime.date.today()
+	USER_SETTINGS = load_settings()
+	earliest_journal_dict: dict = USER_SETTINGS["earliest_journal"]
+	earliest_journal: datetime.date = datetime.date(earliest_journal_dict["year"], earliest_journal_dict["month"], earliest_journal_dict["day"])
+	today: datetime.date = datetime.date.today()
 
-    statistics = []
+	statistics = []
 
-    for year in range(earliest_journal.year, today.year + 1):
-        # use the month end or start from the earliest journal or today if applicable
-        start_month: int = earliest_journal.month if year == earliest_journal.year else 1
-        end_month: int = today.month if year == today.year else 12
-        for month in range(start_month, end_month):
-            # use the day end or start from the earliest journal or today if applicable
-            start_day: int = earliest_journal.day if month == earliest_journal.month and year == earliest_journal.year else 1
-            end_day: int = today.day if month == today.month and year == today.year else calendar.monthrange(year, month)[1] # get days in month
-            for day in range(start_day, end_day):
-                pass
+	for year in range(earliest_journal.year, today.year + 1):
+		# use the month end or start from the earliest journal or today if applicable
+		start_month: int = earliest_journal.month if year == earliest_journal.year else 1
+		end_month: int = today.month if year == today.year else 12
+		for month in range(start_month, end_month):
+			# use the day end or start from the earliest journal or today if applicable
+			start_day: int = earliest_journal.day if month == earliest_journal.month and year == earliest_journal.year else 1
+			end_day: int = today.day if month == today.month and year == today.year else calendar.monthrange(year, month)[1] # get days in month
+			for day in range(start_day, end_day):
+				pass
 
 if __name__ == "__main__":
-    main()
+	main()
