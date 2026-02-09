14b5844290aa6a796854ab104231618898ee7a9b
JayMan146 <jeremywgrover@gmail.com>
2026-02-09 00:25:12 -0800

    added spaces to function arguments

--- gui.py
+++ gui.py
@@ -10,11 +10,11 @@ def main():
     load_settings()
     primary_window.title("Journal System")
 
-    tkinter.Label(primary_window, text="Welcome to the Journal System!", width=35).grid(column=0,row=0)
-    tkinter.Button(primary_window, text="Move Photos", command=move_photos_from_photo_locations, width=20, height=1).grid(column=0,row=1)
-    tkinter.Button(primary_window, text="Create New Entries", command=create_all_recent_missing_entries, width=20, height=1).grid(column=0,row=2)
-    tkinter.Button(primary_window, text="Edit Settings...", command=edit_settings, width=20, height=1).grid(column=0,row=3)
-    tkinter.Button(primary_window, text="Quit", command=primary_window.destroy, width=20, height=1).grid(column=0,row=3)
+    tkinter.Label(primary_window, text="Welcome to the Journal System!", width=35).grid(column=0, row=0)
+    tkinter.Button(primary_window, text="Move Photos", command=move_photos_from_photo_locations, width=20, height=1).grid(column=0, row=1)
+    tkinter.Button(primary_window, text="Create New Entries", command=create_all_recent_missing_entries, width=20, height=1).grid(column=0, row=2)
+    tkinter.Button(primary_window, text="Edit Settings...", command=edit_settings, width=20, height=1).grid(column=0, row=3)
+    tkinter.Button(primary_window, text="Quit", command=primary_window.destroy, width=20, height=1).grid(column=0, row=3)
 
     primary_window.mainloop()
 
