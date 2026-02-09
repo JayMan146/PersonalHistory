ba63a20563ded3787a98a7601dbe27194f71a5d5
JayMan146 <jeremywgrover@gmail.com>
2026-02-09 00:32:31 -0800

    added window for settings

--- gui.py
+++ gui.py
@@ -2,9 +2,13 @@ import tkinter
 from create_base_journal_entries import move_photos_from_photo_locations, create_all_recent_missing_entries, load_settings
 
 primary_window: tkinter.Tk = tkinter.Tk()
+settings_window: tkinter.Tk
 
 def edit_settings():
-    raise NotImplementedError()
+    settings_window = tkinter.Tk()
+    settings_window.title("Journal System Settings")
+    
+    tkinter.Label(settings_window, text="Settings", width=35).grid(column=0, row=0)
 
 def main():
     load_settings()
@@ -14,7 +18,7 @@ def main():
     tkinter.Button(primary_window, text="Move Photos", command=move_photos_from_photo_locations, width=20, height=1).grid(column=0, row=1)
     tkinter.Button(primary_window, text="Create New Entries", command=create_all_recent_missing_entries, width=20, height=1).grid(column=0, row=2)
     tkinter.Button(primary_window, text="Edit Settings...", command=edit_settings, width=20, height=1).grid(column=0, row=3)
-    tkinter.Button(primary_window, text="Quit", command=primary_window.destroy, width=20, height=1).grid(column=0, row=3)
+    tkinter.Button(primary_window, text="Quit", command=primary_window.destroy, width=20, height=1).grid(column=0, row=4)
 
     primary_window.mainloop()
 
