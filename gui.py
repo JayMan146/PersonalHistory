83d9771cda66a6d77e070db306872da46a65a6f0
JayMan146 <jeremywgrover@gmail.com>
2026-02-09 23:28:17 -0800

    import tkinter as tk

--- gui.py
+++ gui.py
@@ -1,20 +1,20 @@
-import tkinter
+import tkinter as tk
 from create_base_journal_entries import move_photos_from_photo_locations, create_all_recent_missing_entries, load_settings
 
-primary_window: tkinter.Tk = tkinter.Tk()
+primary_window: tk.Tk = tk.Tk()
 
 USER_SETTINGS: dict = load_settings()
 
-class MainMenuButton(tkinter.Button):
+class MainMenuButton(tk.Button):
 	def __init__(self, **kwargs):
 		super().__init__(primary_window, width=20, height=1, **kwargs)
 
 def edit_settings():
 	global settings_window
-	settings_window: tkinter.Toplevel = tkinter.Toplevel()
+	settings_window: tk.Toplevel = tk.Toplevel()
 	settings_window.title("Journal System Settings")
 	
-	tkinter.Label(settings_window, text="Settings", width=35).grid(column=0, row=0)
+	tk.Label(settings_window, text="Settings", width=35).grid(column=0, row=0)
 
 def quit():
 	primary_window.destroy()
@@ -22,7 +22,7 @@ def quit():
 def main():
 	primary_window.title("Journal System")
 
-	tkinter.Label(primary_window, text="Welcome to the Journal System!", width=35).grid(column=0, row=0)
+	tk.Label(primary_window, text="Welcome to the Journal System!", width=35).grid(column=0, row=0)
 	MainMenuButton(text="Move Photos", command=move_photos_from_photo_locations).grid(column=0, row=1)
 	MainMenuButton(text="Create New Entries", command=create_all_recent_missing_entries).grid(column=0, row=2)
 	MainMenuButton(text="Edit Settings...", command=edit_settings).grid(column=0, row=3)
