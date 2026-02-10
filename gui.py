89b14edf0518693049e8fdbe6f9cb853c040c236
JayMan146 <jeremywgrover@gmail.com>
2026-02-09 22:59:51 -0800

    made main menu button class

--- gui.py
+++ gui.py
@@ -6,6 +6,10 @@ settings_window: tkinter.Tk
 
 USER_SETTINGS: dict = load_settings()
 
+class MainMenuButton(tkinter.Button):
+	def __init__(self, **kwargs):
+		super().__init__(primary_window, width=20, height=1, **kwargs)
+
 def edit_settings():
 	global settings_window
 	settings_window = tkinter.Tk()
@@ -21,10 +25,10 @@ def main():
 	primary_window.title("Journal System")
 
 	tkinter.Label(primary_window, text="Welcome to the Journal System!", width=35).grid(column=0, row=0)
-	tkinter.Button(primary_window, text="Move Photos", command=move_photos_from_photo_locations, width=20, height=1).grid(column=0, row=1)
-	tkinter.Button(primary_window, text="Create New Entries", command=create_all_recent_missing_entries, width=20, height=1).grid(column=0, row=2)
-	tkinter.Button(primary_window, text="Edit Settings...", command=edit_settings, width=20, height=1).grid(column=0, row=3)
-	tkinter.Button(primary_window, text="Quit", command=quit, width=20, height=1).grid(column=0, row=4)
+	MainMenuButton(text="Move Photos", command=move_photos_from_photo_locations).grid(column=0, row=1)
+	MainMenuButton(text="Create New Entries", command=create_all_recent_missing_entries).grid(column=0, row=2)
+	MainMenuButton(text="Edit Settings...", command=edit_settings).grid(column=0, row=3)
+	MainMenuButton(text="Quit", command=quit).grid(column=0, row=4)
 
 	primary_window.mainloop()
 
