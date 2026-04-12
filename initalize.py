7e5b8aa7a6eeca725a84ddd905acd02f1b7dc166
JayMan146 <jeremywgrover@gmail.com>
2026-04-11 20:20:46 -0700

    Add Message That Is Written to New Journals
    Adds a message for new journals when initalize.py is run,
    giving more information on the program.

--- initalize.py
+++ initalize.py
@@ -5,14 +5,21 @@ import settings
 def intialize_journals() -> None:
 	"""Creates a single entry for today."""
 	
-    # get settings ready for new use
+	# get settings ready for new use
 	settings.load_settings_profile("settings_default")
 	settings.create_default_settings_profile_txt()
+
+	# to allow the code later on that writes the message in the journal to be positioned better
+	settings.USER_SETTINGS["format"]["writing_lines"] = 0
 	
-    # create journal entry for today
+	# create journal entry for today
 	today = datetime.date.today()
 	entry = journal_system.generate_entry(today, settings.USER_SETTINGS["format"]["header_suffix"])
 	journal_system.write_entry(entry, today)
+	
+	# write start message
+	with open(journal_system.convert_date_to_journal_path(today)[1], "a") as initalized_entry:
+		initalized_entry.write("Welcome to your first journal! To use the program more from here, check out the [Usage section of the README](../PersonalHistory/README#usage), and other sections. If you want your journal to look different, try making your own settings profile. Feel free to delete this line and starting writing your Personal History.\n")
 	
 if __name__ == "__main__":
 	intialize_journals()
