9fcddca363a8e9c37742ad49b29db6e681c2fd1f
JayMan146 <jeremywgrover@gmail.com>
2026-04-12 12:51:05 -0700

    Make initialize.py Print Success Message

--- initialize.py
+++ initialize.py
@@ -18,8 +18,15 @@ def intialize_journals() -> None:
 	journal_system.write_entry(entry, today)
 	
 	# write start message
-	with open(journal_system.convert_date_to_journal_path(today)[1], "a") as initialized_entry:
+	first_journal_path: str = journal_system.convert_date_to_journal_path(today)[1]
+	with open(first_journal_path, "a") as initialized_entry:
 		initialized_entry.write("Welcome to your first journal! To use the program more from here, check out the [Usage section of the README](../PersonalHistory/README#usage), and other sections. If you want your journal to look different, try making your own settings profile. Feel free to delete this line and starting writing your Personal History.\n")
+		
+	journal_system.output_to_console_by_level([
+		settings.ConsoleOutput(
+			[settings.ConsoleOutputLevels.NONE, settings.ConsoleOutputLevels.MINIMUM, settings.ConsoleOutputLevels.MEDIUM, settings.ConsoleOutputLevels.MAXIMUM], f"The journal system has been successfully initialized! Go check out your first file at \"{first_journal_path}\"!"
+		)
+	])
 	
 if __name__ == "__main__":
 	intialize_journals()
