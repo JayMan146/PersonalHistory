ad4d83af38fd6613fc286a986ae81fbcee091630
JayMan146 <jeremywgrover@gmail.com>
2026-04-12 17:00:34 -0700

    Add Beginnings of Settings Editor in GUI

--- gui.py
+++ gui.py
@@ -4,25 +4,31 @@ import settings
 
 primary_window: tk.Tk = tk.Tk()
 
-USER_SETTINGS: dict = settings.load_current_selected_profile()
-
 class MainMenuButton(tk.Button):
 	def __init__(self, **kwargs):
 		super().__init__(primary_window, width=20, height=1, **kwargs)
 
 def edit_settings():
+	def create_new_profile():
+		pass
+
 	settings_window: tk.Toplevel = tk.Toplevel()
-	settings_window.title("Journal System Settings")
+	settings_window.title("Personal History Settings")
 	
-	tk.Label(settings_window, text="Settings", width=35).grid(column=0, row=0)
+	tk.Button(settings_window, command=create_new_profile, text="+ New Profile",width=15).grid(column=0, row=0)
+	tk.Entry(settings_window).grid(column=1, row=0)
+	tk.Label(settings_window, text="Profile: ", width=15).grid(column=0, row=1)
+	
+	selectable_profiles: list = settings.get_all_profiles()
+	tk.OptionMenu(settings_window, tk.StringVar(value=settings.get_selected_profile()), *selectable_profiles).grid(column=1, row=1)
 
 def quit():
 	primary_window.destroy()
 
 def gui():
-	primary_window.title("Journal System")
+	primary_window.title("Personal History")
 
-	tk.Label(primary_window, text="Welcome to the Journal System!", width=35).grid(column=0, row=0)
+	tk.Label(primary_window, text="Welcome to Personal History!", width=35).grid(column=0, row=0)
 	MainMenuButton(text="Move Photos", command=journal_system.move_photos_from_photo_locations).grid(column=0, row=1)
 	MainMenuButton(text="Create New Entries", command=journal_system.create_all_recent_missing_entries).grid(column=0, row=2)
 	MainMenuButton(text="Edit Settings...", command=edit_settings).grid(column=0, row=3)
